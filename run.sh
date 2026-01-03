7aef6e39c4817d0e530a0984de461345f21174c4
SalarAlo <salarmanan8@gmail.com>
2026-01-03 06:53:10 +0100

    BUILD(gitignore): reapply ignore rules

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "-e" ]; then
-    echo "Launching Editor..."
-    ./build/bin/Debug-Linux/OrigoEditor
-else
-    echo "Launching Game..."
-    ./build/bin/Debug-Linux/OrigoGame
-fi
